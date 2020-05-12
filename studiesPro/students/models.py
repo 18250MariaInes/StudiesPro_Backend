acb36716767425ad0c6d9698644d05b58283cb7c
Maria Vasquez <maria.ines.vf@gmail.com>
2020-05-12 10:37:29 -0600

    inicio de backend

---
+++ studiesPro/students/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class StudentManager(BaseUserManager):
+    def create_user(self, email, name, lastname, carne, sship, password=None):
+            if not email:
+                    raise ValueError('Users must have an email address')
+            if not name:
+                    raise ValueError('Users must have a name')
+            if not lastname:
+                    raise ValueError('Users must have a lastname')
+            if not carne:
+                    raise ValueError('Users must have a carne')
+            if not sship:
+                    raise ValueError('Users must have a sship')
+
+            user = self.model(
+                email=self.normalize_email(email),
+                name=name,
+                lastname=lastname,
+                carne=carne,
+                sship=sship
+            )
+
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+    
+    def create_superuser(self, email, name, lastname, carne, sship, password):
+            user = self.create_user(
+                email=self.normalize_email(email),
+                password=password,
+                name=name,
+                lastname=lastname,
+                carne=carne,
+                sship=sship
+            )
+            user.is_admin = True
+            user.is_staff = True
+            user.is_superuser = True
+            user.save(using=self._db)
+            return user
+
+
+
+class Student(AbstractBaseUser):
+        email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
+        username 				= models.CharField(max_length=30)
+        date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+        last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
+        is_admin				= models.BooleanField(default=False)
+        is_active				= models.BooleanField(default=True)
+        is_staff				= models.BooleanField(default=False)
+        is_superuser			= models.BooleanField(default=False)
+        carne                   = models.IntegerField(null=False)
+        name                    = models.CharField(max_length=200)
+        lastname                = models.CharField(max_length=200)
+        sship                   = models.IntegerField(null=False)
+
+        USERNAME_FIELD='email'
+        REQUIRED_FIELD=['name', 'lastname', 'carne', 'sship']
+
+        objects= StudentManager()
+
+        def __str__(self):
+            return self.email
+
+        def has_perm(self, perm, obj=None):
+            return self.is_admin
+
+        def has_module_perms(self, app_label):
+            return True
